04b07cf2a76124879cb02da202f24c03a958a4c1
acreech2 <acreech2@07c14406-049b-48c7-ae0a-80bc69f9f8d2>
2016-07-27 17:17:19 +0000

    Changes: - Corrected ele2grad_mat calcs in assembly - Fixed Van Driest wall equations for LES

--- branches/tidal/assemble/DG_LES.F90
+++ branches/tidal/assemble/DG_LES.F90
@@ -89,7 +89,7 @@ contains
         logical :: have_van_driest, have_reference_density
 
         ! Constants for Van Driest damping equation
-        real, parameter :: A_plus=25.0, pow_m=2.0
+        real, parameter :: A_plus=25.6, pow_m=2.0
 
         print*, "calc_dg_sgs_scalar_viscosity"
 
@@ -216,8 +216,8 @@ contains
         if(have_van_driest) then
             do n=1, num_nodes
                 u_grad_node = u_grad%val(:,:, n)
-                y_plus = sqrt(norm2(u_grad_node) * rho * mu) * dist_to_wall%val(n)
-                vd_damping =(( 1- exp(-y_plus/A_plus))**pow_m)*van_scale+(1-van_scale)
+                y_plus = sqrt(norm2(u_grad_node+transpose(u_grad_node)) * rho) * dist_to_wall%val(n)
+                vd_damping = 1.0 - exp(-y_plus/A_plus)
 
                 call set(sgs_visc, n, &
                     vd_damping * rho*0.5*(node_sum(n) / node_visits(n) &
