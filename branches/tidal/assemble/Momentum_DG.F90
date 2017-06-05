3cad08d9dd42515b9f5ca8a691f5cebfd7800b5e
acreech2 <acreech2@07c14406-049b-48c7-ae0a-80bc69f9f8d2>
2017-06-05 20:33:12 +0000

    Adding James' corrections to partial-stress form of DG, plus beginnings of Roman et al DG LES proper.

--- branches/tidal/assemble/Momentum_DG.F90
+++ branches/tidal/assemble/Momentum_DG.F90
@@ -24,14 +24,9 @@
 !    License along with this library; if not, write to the Free Software
 !    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 !    USA
+
 #include "fdebug.h"
-
 #include "compile_opt_defs.h"
-
-#define NLOC 4
-#define NDIM 3
-
-
 
 module momentum_DG
     ! This module contains the Discontinuous Galerkin form of the momentum
