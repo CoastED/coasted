15e86d7f1504447731e9c11255af0d02b061e5b7
angus <angus@07c14406-049b-48c7-ae0a-80bc69f9f8d2>
2020-09-01 05:07:16 +0000

    Memory leak - deallocate v_grad and w_grad too!
    git-svn-id: svn+ssh://clubbedtodeath.net/home/angus/svn/coasted/master@178 07c14406-049b-48c7-ae0a-80bc69f9f8d2

--- assemble/DG_LES.F90
+++ assemble/DG_LES.F90
@@ -714,6 +714,8 @@ contains
         call halo_update(sgs_visc)
 
         call deallocate(u_grad)
+        call deallocate(v_grad)
+        call deallocate(w_grad)
         deallocate( S, dudx_n, del_gradu, B, u_cg_ele, dx )
 
         deallocate(node_filter_lengths)
