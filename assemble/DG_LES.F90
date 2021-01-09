f5baa622079d267825fb9d777d9ca90f3dab007f
angus <angus@07c14406-049b-48c7-ae0a-80bc69f9f8d2>
2021-01-09 14:20:36 +0000

    Minor correction to sgs limiting for DG QR model.
    git-svn-id: svn+ssh://clubbedtodeath.net/home/angus/svn/coasted/master@201 07c14406-049b-48c7-ae0a-80bc69f9f8d2

--- assemble/DG_LES.F90
+++ assemble/DG_LES.F90
@@ -692,8 +692,8 @@ contains
            end if
 
             ! Limiter
-            if(sgs_visc_val > mu*10e4) then
-               if(sgs_visc%val(n) < mu*10e4) then
+            if(sgs_visc_val > mu*1e4) then
+               if(sgs_visc%val(n) < mu*1e4) then
                   sgs_visc_val=sgs_visc%val(n)
                else
                   sgs_visc_val=0.
