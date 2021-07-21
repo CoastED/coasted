4bd41b2e20034f7ad2b6a19f37886cac88d37799
angus <angus@07c14406-049b-48c7-ae0a-80bc69f9f8d2>
2021-07-21 19:11:42 +0000

    Added DG assembly speed-ups, which have been thoroughly tested.
    git-svn-id: svn+ssh://clubbedtodeath.net/home/angus/svn/coasted/master@277 07c14406-049b-48c7-ae0a-80bc69f9f8d2

--- assemble/Construct_Momentum_Element_DG_opt.F90
+++ assemble/Construct_Momentum_Element_DG_opt.F90
@@ -186,6 +186,8 @@ subroutine construct_momentum_elements_dg_opt( ele, big_m, rhs, &
     integer :: nmat_d1,nmat_d2
 
 
+!    real :: visc2norm, tent_visc_mat( mesh_dim(U), mesh_dim(U), ele_loc(U, ele), ele_loc(U, ele) )
+
 
      ! ========== INTERNAL INTERFACE VARIABLES ==========
 
@@ -981,31 +983,60 @@ subroutine construct_momentum_elements_dg_opt( ele, big_m, rhs, &
             !                        du_t, detwei)
 
             ! Optimised (unrolled)
+            ! do idim=1, opDim
+            !     sh_dt_temp = 0.0
+
+            !     do gi=1,opNgi
+            !         do concurrent(iloc=1:opNloc, jloc=1:opNloc)
+            !             sh_dt_temp (iloc,jloc)=sh_dt_temp (iloc,jloc) &
+            !                 + dot_product( &
+            !                 matmul(du_t(iloc,gi,:), visc_ele_quad(:,:,gi)), &
+            !                 du_t(jloc,gi,:))*detwei(gi)
+            !         end do
+            !     end do
+            !     Viscosity_mat(idim,idim,:opNloc,:opNloc) = sh_dt_temp
+            !  end do
+
+            ! Faster (?) way of doing things?
+
             do idim=1, opDim
                 sh_dt_temp = 0.0
 
-                do gi=1,opNgi
-                    do concurrent(iloc=1:opNloc, jloc=1:opNloc)
-                        sh_dt_temp (iloc,jloc)=sh_dt_temp (iloc,jloc) &
-                            + dot_product( &
-                            matmul(du_t(iloc,gi,:), visc_ele_quad(:,:,gi)), &
-                            du_t(jloc,gi,:))*detwei(gi)
+                do iloc=1, opNloc
+
+#if opVelDeg == 1
+                    ! Optimisation for linear elements
+                    matmul_dut_visc = matmul(du_t(iloc,1,:), visc_ele_quad(:,:,1))
+#endif
+                    do gi=1, opNgi
+#if opVelDeg > 1
+                        ! For higher-order elements
+                        matmul_dut_visc = matmul(du_t(iloc,gi,:), visc_ele_quad(:,:,gi))
+#endif
+                        do jloc=1, opNloc
+                            sh_dt_temp (iloc,jloc)=sh_dt_temp (iloc,jloc) &
+                                + dot_product( &
+                                matmul_dut_visc, &
+                                du_t(jloc,gi,:))*detwei(gi)
+                        end do
                     end do
                 end do
+
+                ! tent_visc_mat(idim,idim,:opNloc,:opNloc) = sh_dt_temp
                 Viscosity_mat(idim,idim,:opNloc,:opNloc) = sh_dt_temp
-             end do
+            end do
 
 
-!            do idim = 1 , opDim
-!                visc2norm = norm2(reshape( ( Viscosity_mat(idim, idim, :, :)-tent_visc_mat(idim, idim, :, :) ), (/16/)))
-!
-!                if(visc2norm > 10e-7) then
-!                    print*, "visc2norm:", visc2norm
-!                    FLAbort("visc2norm > 10e-7")
-!                end if
-!            end do
+            ! ! Compare old and new viscosity matrices
+            ! do idim = 1 , opDim
+            !     visc2norm = norm2(reshape( ( Viscosity_mat(idim, idim, :, :)-tent_visc_mat(idim, idim, :, :) ), (/16/)))
 
-!            end if
+            !     if(visc2norm > 10e-7) then
+            !         print*, "visc2norm:", visc2norm
+            !         FLAbort("visc2norm > 10e-7")
+            !     end if
+            ! end do
+
 
             if(partial_stress) then
                                 ! This is where to stick the partial stress stuff for LES
